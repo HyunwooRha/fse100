5524736231ce4a5defb56806ecdd8427d8a67486
HyunwooRha <hyunwoorha@gmail.com>
2022-11-14 17:20:23 -0800

    Update final doc.m

--- final doc.m
+++ final doc.m
@@ -12,7 +12,7 @@ droppedoff = false;
 
 function getPath()
     % return a list of all connections
-    % aka spin around 360* to see if there are any connections
+    % aka spin around to see if there are any connections
 end
 
 function checkColors()
@@ -21,21 +21,41 @@ function checkColors()
     % also need to make sure finish is set to current on 
 end
 
-while test1
-    checkColors();
-    graph[current] = getPath();
-    current = current + 1;
-    turn(turnDirection);
-    goForward();
-
+function d = turnDirection()
+    if (!getEast):
+        d = 2;
+    elseif (!getNorth):
+        d = 1;
+    else:
+        d = 4;
+    end
+    return d;
 end
 
-while test2
-    % follow directions given by find_path
-
+function bool = getNorth()
+    
+    return(bool);
+end
+function bool = getEast()
+    
+    return(bool);
+end
+function bool = getSouth()
+    
+    return(bool);
+end
+function bool = getWest()
+    
+    return(bool);
 end
 
-function find_path(graph, start, finish, paths=[])
+function c = getColor()
+    
+    return(c);
+end
+
+
+function path = find_path(graph, start, finish, paths=[])
     path = paths + [start]
     if start == finish
         return path
@@ -54,14 +74,16 @@ function find_path(graph, start, finish, paths=[])
     return None
 end
 
-% def find_path(graph, start, end, path=[]):
-%         path = path + [start]
-%         if start == end:
-%             return path
-%         if not graph.has_key(start):
-%             return None
-%         for node in graph[start]:
-%             if node not in path:
-%                 newpath = find_path(graph, node, end, path)
-%                 if newpath: return newpath
-%         return None
+while test1
+    checkColors();
+    graph[current] = getPath();
+    turn(turnDirection);
+    current = current + 1;
+    goForward();
+
+end
+
+while test2
+    % follow directions given by find_path
+
+end
